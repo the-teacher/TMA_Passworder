06942e6ac0970cb5d9551b9d9b43b568836fc61b
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-10 20:20:20 +0300

    Install Prettier

---
+++ _Frontend/.prettierrc.js
@@ -0,0 +1,13 @@
+export default {
+  semi: true,
+  trailingComma: "all",
+  singleQuote: false,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: "lf",
+  arrowParens: "always",
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  quoteProps: "as-needed",
+  useTabs: false,
+};
