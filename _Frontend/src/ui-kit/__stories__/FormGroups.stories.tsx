8a2db97ad8459f7b8d31dfec8e0c90d4eb74a98f
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-16 16:35:36 +0300

    form group

---
+++ _Frontend/src/ui-kit/__stories__/FormGroups.stories.tsx
@@ -0,0 +1,68 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import "@ui-kit/form-groups.scss";
+import "@ui-kit/form-inputs.scss";
+import "@ui-kit/text-styles.scss"; // Assuming text styles are defined here
+
+const meta: Meta = {
+  title: "4-UI-Kit/FormGroups",
+  component: "div",
+  parameters: {
+    docs: { disable: true },
+    layout: "fullscreen"
+  }
+};
+
+export default meta;
+type Story = StoryObj;
+
+// Form group story
+export const FormGroupExample: Story = {
+  render: () => (
+    <div className="p16">
+      <div className="form-group">
+        <label className="form-group__label text--dark" htmlFor="username">
+          Username
+        </label>
+        <input
+          className="form-group__input form-input"
+          id="username"
+          type="text"
+          placeholder="Enter your username"
+        />
+        <div className="form-group__info text--info text--small">
+          Please enter a unique username.
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label className="form-group__label text--dark" htmlFor="email">
+          Email
+        </label>
+        <input
+          className="form-group__input form-input"
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+        />
+        <div className="form-group__info text--danger text--small">
+          Please enter a valid email address.
+        </div>
+      </div>
+
+      <div className="form-group">
+        <label className="form-group__label text--dark" htmlFor="password">
+          Password
+        </label>
+        <input
+          className="form-group__input form-input"
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+        />
+        <div className="form-group__info text--success text--small">
+          Your password is strong.
+        </div>
+      </div>
+    </div>
+  )
+};
