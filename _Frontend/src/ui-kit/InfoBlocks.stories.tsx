4e1973813b5cea6f66c0c9f1c2d27791a90c0c96
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-16 08:59:00 +0300

    UI kit info block

---
+++ _Frontend/src/ui-kit/InfoBlocks.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import "./info-blocks.scss";
+
+const meta: Meta = {
+  title: "4-UI-Kit/InfoBlocks",
+  component: "div",
+  parameters: {
+    docs: { disable: true },
+    layout: "fullscreen"
+  }
+};
+
+export default meta;
+type Story = StoryObj;
+
+// All info blocks story
+export const AllInfoBlocks: Story = {
+  render: () => (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "16px",
+        padding: "16px"
+      }}
+    >
+      <div className="info">Plain Info Block</div>
+      <div className="info info--primary">Primary Info Block</div>
+      <div className="info info--secondary">Secondary Info Block</div>
+      <div className="info info--success">Success Info Block</div>
+      <div className="info info--warning">Warning Info Block</div>
+      <div className="info info--danger">Danger Info Block</div>
+      <div className="info info--light">Light Info Block</div>
+      <div className="info info--dark">Dark Info Block</div>
+    </div>
+  )
+};
