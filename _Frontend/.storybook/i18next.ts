94b05a146eb2302dd66c34372c7726e72fd6d31b
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-20 14:29:17 +0300

    Fix story for i18n

--- _Frontend/.storybook/i18next.ts
+++ _Frontend/.storybook/i18next.ts
@@ -1,24 +1,33 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { loadAppLocales } from "../src/i18n/utils";
+import enSchema from "../src/i18n/locales.schema.en.json";
+import ruSchema from "../src/i18n/locales.schema.ru.json";
 
-const DEFAULT_LANGUAGE = "en"; // For Storybook we'll default to English
+const DEFAULT_LANGUAGE = import.meta.env.VITE_DEFAULT_LANGUAGE || "en";
 
-const initI18n = async () => {
-  const resources = await loadAppLocales();
+type LocaleSchema = typeof enSchema;
+type Resources = Record<string, LocaleSchema>;
 
-  await i18n.use(initReactI18next).init({
-    lng: DEFAULT_LANGUAGE,
-    fallbackLng: DEFAULT_LANGUAGE,
-    interpolation: {
-      escapeValue: false
-    },
-    resources,
-    supportedLngs: ["en", "ru"]
-  });
+const resources: Resources = {
+  en: enSchema,
+  ru: ruSchema
 };
 
-// Initialize i18n
-initI18n();
+const Namespaces = Object.keys(
+  resources[DEFAULT_LANGUAGE as keyof Resources] || {}
+);
+
+i18n.use(initReactI18next).init({
+  fallbackLng: DEFAULT_LANGUAGE,
+  lng: DEFAULT_LANGUAGE,
+  resources,
+  defaultNS: "translations",
+  fallbackNS: Namespaces,
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+i18n.languages = ["en", "ru"];
 
 export default i18n;
