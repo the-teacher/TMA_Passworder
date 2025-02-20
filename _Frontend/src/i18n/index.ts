aacefd0b1228d55c28a77dbfa39fec0f26d294df
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-20 12:12:01 +0300

    Locales for app

--- _Frontend/src/i18n/index.ts
+++ _Frontend/src/i18n/index.ts
@@ -1,26 +1,29 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import { loadAppLocales } from "./utils";
+import enSchema from "./locales.schema.en.json";
+import ruSchema from "./locales.schema.ru.json";
 
 const DEFAULT_LANGUAGE = import.meta.env.VITE_DEFAULT_LANGUAGE || "ru";
 
-const initI18n = async () => {
-  const resources = await loadAppLocales();
+type LocaleSchema = typeof enSchema;
+type Resources = Record<string, LocaleSchema>;
 
-  await i18n.use(initReactI18next).init({
-    fallbackLng: DEFAULT_LANGUAGE,
-    lng: DEFAULT_LANGUAGE,
-    resources,
-    defaultNS: "translations",
-    fallbackNS: Object.keys(resources[DEFAULT_LANGUAGE] || {}),
-    interpolation: {
-      escapeValue: false
-    }
-  });
-
-  i18n.languages = ["en", "ru"];
+const resources: Resources = {
+  en: enSchema,
+  ru: ruSchema
 };
 
-initI18n();
+i18n.use(initReactI18next).init({
+  fallbackLng: DEFAULT_LANGUAGE,
+  lng: DEFAULT_LANGUAGE,
+  resources,
+  defaultNS: "translations",
+  fallbackNS: Object.keys(resources[DEFAULT_LANGUAGE as keyof Resources] || {}),
+  interpolation: {
+    escapeValue: false
+  }
+});
+
+i18n.languages = ["en", "ru"];
 
 export default i18n;
