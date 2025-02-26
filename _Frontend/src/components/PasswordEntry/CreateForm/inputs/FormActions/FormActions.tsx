c921154b389697e2e8fff31e3881f63df1b676ec
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-26 14:21:46 +0300

    Add modal confirmation

--- _Frontend/src/components/PasswordEntry/CreateForm/inputs/FormActions/FormActions.tsx
+++ _Frontend/src/components/PasswordEntry/CreateForm/inputs/FormActions/FormActions.tsx
@@ -1,14 +1,40 @@
 import { useTranslation } from "react-i18next";
 import { useFormContext } from "react-hook-form";
+import { useAppModal } from "@components/AppModal";
 import type { FormData } from "../../validationSchema";
 import "./styles.scss";
 
 const FormActions = () => {
   const { t: c } = useTranslation("common");
+  const { t } = useTranslation("CreatePasswordEntryForm");
   const {
     formState: { isValid, isSubmitting },
     reset
   } = useFormContext<FormData>();
+
+  const confirmReset = useAppModal({
+    title: t("modals.resetForm.title"),
+    size: "small",
+    children: ({ close }) => (
+      <div>
+        <p className="text--center">{t("modals.resetForm.message")}</p>
+        <div className="form-group--actions mt16">
+          <button className="btn btn--secondary" onClick={close}>
+            {t("modals.resetForm.cancel")}
+          </button>
+          <button
+            className="btn btn--primary"
+            onClick={() => {
+              reset();
+              close();
+            }}
+          >
+            {t("modals.resetForm.confirm")}
+          </button>
+        </div>
+      </div>
+    )
+  });
 
   return (
     <div className="form-group--actions form-group--actions__with-jumbo">
@@ -22,10 +48,11 @@ const FormActions = () => {
       <button
         type="button"
         className="btn btn--secondary"
-        onClick={() => reset()}
+        onClick={confirmReset.open}
       >
         {c("reset")}
       </button>
+      {confirmReset.modal}
     </div>
   );
 };
