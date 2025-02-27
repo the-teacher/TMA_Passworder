827f052ef634ffa6ea0b11a8488d20f1a6943e99
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-02-27 13:54:15 +0300

    clean ups

--- _Frontend/src/components/PasswordEntry/CreateForm/CreatePasswordEntryFormView.tsx
+++ _Frontend/src/components/PasswordEntry/CreateForm/CreatePasswordEntryFormView.tsx
@@ -5,12 +5,12 @@ import PasswordInput from "./inputs/PasswordInput";
 import NotesInput from "./inputs/NotesInput";
 import FormActions from "./inputs/FormActions";
 
+import "@ui-kit/info-blocks.scss";
+import "@ui-kit/text-styles.scss";
+import "@ui-kit/spaces.scss";
+import "@ui-kit/form-groups.scss";
 import "@ui-kit/form-inputs.scss";
 import "@ui-kit/buttons.scss";
-import "@ui-kit/form-groups.scss";
-import "@ui-kit/text-styles.scss";
-import "@ui-kit/info-blocks.scss";
-import "@ui-kit/spaces.scss";
 
 export type Props = {
   onSubmit: (e: React.FormEvent) => void;
