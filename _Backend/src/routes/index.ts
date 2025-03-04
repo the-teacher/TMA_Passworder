a272ab90d5926fa97c3f324246fa9de4cb0194cd
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-03-04 13:25:20 +0300

    Start backend development

--- _Backend/src/routes/index.ts
+++ _Backend/src/routes/index.ts
@@ -1,3 +1,7 @@
-import { root } from '@the-teacher/the-router'
+import { root, get, resources } from '@the-teacher/the-router'
 
 root('index/index')
+
+// User routes
+get('/users/exists/:login', 'users/exists')
+resources('users', { except: ['new'] })
