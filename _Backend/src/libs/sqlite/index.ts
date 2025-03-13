3d1604f8719b4aabcbf8e03a4149f12ddc07fe66
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-03-13 18:18:59 +0300

    cleanup

--- _Backend/src/libs/sqlite/index.ts
+++ _Backend/src/libs/sqlite/index.ts
@@ -1,5 +1,4 @@
 export { getDatabase } from './getDatabase';
 export { runSqlQuery } from './runSqlQuery';
 export { runQuery } from './runQuery';
-export { runQueries } from './runQueries';
 export { runInTransaction } from './runInTransaction';
--- _Backend/src/libs/sqlite/runSqlQuery.ts
+++ _Backend/src/libs/sqlite/runSqlQuery.ts
@@ -1,8 +1,13 @@
 import sqlite3 from 'sqlite3';
 
-export const runSqlQuery = (db: sqlite3.Database, query: string): Promise<void> => {
+/**
+ * Execute a SQL query on a database connection
+ * @param db SQLite database connection
+ * @param sql SQL query to execute
+ */
+export const runSqlQuery = (db: sqlite3.Database, sql: string): Promise<void> => {
   return new Promise((resolve, reject) => {
-    db.run(query, (err) => {
+    db.run(sql, (err) => {
       if (err) {
         reject(err);
         return;
