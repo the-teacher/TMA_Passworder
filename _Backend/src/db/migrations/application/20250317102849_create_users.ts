6278938fca670f06ab646d763c63bf66134f8fae
Ilya N. Zykin <zykin-ilya@ya.ru>
2025-03-17 14:04:46 +0300

    update migration

--- _Backend/src/db/migrations/application/20250317102849_create_users.ts
+++ _Backend/src/db/migrations/application/20250317102849_create_users.ts
@@ -1,4 +1,4 @@
-import { runQuery } from '@libs/sqlite';
+import { runQuery } from '@libs/sqlite/runQuery';
 
 /**
  * Migration: create_users
@@ -11,6 +11,7 @@ export const up = async (dbPath: string): Promise<void> => {
     dbPath,
     `CREATE TABLE users (
         id             INTEGER PRIMARY KEY AUTOINCREMENT,
+        system_id      TEXT NOT NULL UNIQUE, -- Unique string ID (12 chars) used for user data directory
         name           TEXT NOT NULL,
         email          TEXT UNIQUE NOT NULL,
         is_active      INTEGER DEFAULT 0 CHECK (is_active IN (0, 1)),  -- 0 = Not Activated, 1 = Activated
